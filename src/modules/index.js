95596c6f1aa531c49170dbbe9e9ddce478c55eb9
admin <admin@zhangyujie.local>
2019-04-03 16:43:45 +0800

    增加部分打包索引

---
+++ src/modules/index.js
@@ -0,0 +1,9 @@
+import popup from './../modules/popup';
+import picker from './../modules/picker';
+import cascadePicker from './../modules/cascade-picker';
+
+export default {
+  popup,
+  picker,
+  cascadePicker
+}
